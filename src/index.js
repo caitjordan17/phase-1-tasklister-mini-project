b5fe3e9fced4fa255108ef398a90728578138c9e
Caitlin Jordan <jordan.cait17@gmail.com>
2023-02-20 13:27:03 -0800

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let task = document.querySelector('#new-task-description')
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    buildToDo(task.value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  console.log(todo)
+  let btn = document.createElement('button')
+  let p = document.createElement('p')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = ` ${todo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
